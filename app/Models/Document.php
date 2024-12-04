14a09fb13c27e71caf14002e2b6c08257b43bd7c
mezentsevs <mezentsevs@rambler.ru>
2024-12-04 19:01:14 +0400

    Add user has many documents and document belongs to user relations

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -3,10 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Document extends Model
 {
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
     public function images(): HasMany
     {
         return $this->hasMany(Image::class);
