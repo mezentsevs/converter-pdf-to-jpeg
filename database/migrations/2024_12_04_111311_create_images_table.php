4b1d1ac150c05f24a2e441389ed55c37932173fa
mezentsevs <mezentsevs@rambler.ru>
2024-12-04 15:40:38 +0400

    Add create documents and images tables migrations

---
+++ database/migrations/2024_12_04_111311_create_images_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('document_id')->constrained()->cascadeOnDelete();
+            $table->string('filename');
+            $table->string('type');
+            $table->integer('size');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('images');
+    }
+};
