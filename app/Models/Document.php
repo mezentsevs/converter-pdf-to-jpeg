d73cb0114dd495c4d235f50c38cb3f9b27023d3f
mezentsevs <mezentsevs@rambler.ru>
2024-12-04 16:44:29 +0400

    Add document has many images and image belongs to document relations

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Document extends Model
 {
-    //
+    public function images(): HasMany
+    {
+        return $this->hasMany(Image::class);
+    }
 }
--- app/Models/Image.php
+++ app/Models/Image.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Image extends Model
 {
-    //
+    public function document(): BelongsTo
+    {
+        return $this->belongsTo(Document::class);
+    }
 }
