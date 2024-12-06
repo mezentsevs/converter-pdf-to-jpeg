a0ce0789e816a51c878cde5d0e7cf0839eaffc82
mezentsevs <mezentsevs@rambler.ru>
2024-12-06 21:14:04 +0400

    Add form for document store action

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
+            {{ config('app.name', 'Laravel') }}
         </h2>
     </x-slot>
 
@@ -9,7 +9,20 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    <form method="POST" action="{{ route('document.store') }}" enctype="multipart/form-data">
+                        @csrf
+
+                        <div class="inline-block">
+                            <x-input-label for="document" :value="__('documents.choose')" />
+                            <input type="hidden" name="MAX_FILE_SIZE" value="{{ config('uploads.post.max_file_size') }}">
+                            <x-file-input id="document" type="file" name="document" accept="application/pdf" required />
+                            <x-input-error :messages="$errors->get('document')" class="mt-2" />
+                        </div>
+
+                        <x-primary-button class="ml-5">
+                            {{ __('documents.convert') }}
+                        </x-primary-button>
+                    </form>
                 </div>
             </div>
         </div>
