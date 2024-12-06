ebcca911d0f78c2128a3242196bb40b830c27981
mezentsevs <mezentsevs@rambler.ru>
2024-12-06 19:00:27 +0400

    Add documents lang file

---
+++ lang/en/documents.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Documents Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the custom lines for documents.
+    |
+    */
+
+    'choose' => 'Choose your document for conversion',
+    'convert' => 'Convert',
+
+];
