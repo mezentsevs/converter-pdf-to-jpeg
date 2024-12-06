dbbd752487035f775e7a500803407a077c153783
mezentsevs <mezentsevs@rambler.ru>
2024-12-06 21:26:35 +0400

    Update default app name

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ config('app.name', 'Laravel') }}
+            {{ config('app.name', 'Converter') }}
         </h2>
     </x-slot>
 
