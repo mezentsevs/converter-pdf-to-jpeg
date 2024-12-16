e37f8f2c8ffd241203a7146d10a07749f1877e88
mezentsevs <mezentsevs@rambler.ru>
2024-12-16 21:17:15 +0400

    Update dashboard blade

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,19 +6,19 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-3xl mx-auto">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <form method="POST" action="{{ route('document.store') }}" enctype="multipart/form-data">
+                    <form class="flex justify-between" method="POST" action="{{ route('document.store') }}" enctype="multipart/form-data">
                         @csrf
 
-                        <div class="inline-block">
+                        <div class="inline-block sm:ml-5 lg:ml-5">
                             <x-input-label for="document" :value="__('documents.uploads.label')" />
                             <x-file-input id="document" type="file" name="document" accept="application/pdf" required />
                             <x-input-error :messages="$errors->get('document')" class="mt-2" />
                         </div>
 
-                        <x-primary-button class="ml-5">
+                        <x-primary-button class="mt-5 sm:m-5 lg:m-5 h-8">
                             {{ __('documents.uploads.button') }}
                         </x-primary-button>
                     </form>
