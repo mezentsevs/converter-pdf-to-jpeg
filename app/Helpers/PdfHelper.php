ae3af85265831de089c27c0987839d6cdaab50c4
mezentsevs <mezentsevs@rambler.ru>
2024-12-19 14:33:50 +0400

    Add pdf helper and count pages method

---
+++ app/Helpers/PdfHelper.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Helpers;
+
+class PdfHelper
+{
+    public static function countPages(string $path): int
+    {
+        exec("pdfinfo \"$path\"", $output);
+
+        $count = 0;
+        foreach ($output as $line) {
+            if (preg_match("/Pages:\s*(\d+)/i", $line, $matches)) {
+                $count = (int) $matches[1];
+                break;
+            }
+        }
+
+        return $count;
+    }
+}
