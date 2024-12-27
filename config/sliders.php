143db2807d456cddb529da1e87ca0a8fb3ec1659
mezentsevs <mezentsevs@rambler.ru>
2024-12-27 14:39:08 +0400

    Add sliders images directory to config and env.example

---
+++ config/sliders.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Sliders parameters
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify sliders parameters.
+    |
+    */
+
+    'images' => [
+        'directory' => env('SLIDERS_IMAGES_DIRECTORY', 'images'),
+    ],
+
+];
