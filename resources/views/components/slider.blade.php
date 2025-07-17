17063bf44a137a0e3fe975badfe1935a3a3b986f
mezentsevs <mezentsevs@rambler.ru>
2025-07-17 17:23:41 +0400

    Refactor slider component

--- resources/views/components/slider.blade.php
+++ resources/views/components/slider.blade.php
@@ -1,31 +1,27 @@
-<div id="slider" class="py-2 hidden">
-    <div class="max-w-3xl mx-auto">
-        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg text-center">
-            <figure class="mt-5 flex justify-evenly items-center">
-                <x-secondary-button id="prev" class="m-5 h-8" onclick="Slider.prev()">
-                    &lt;
-                </x-secondary-button>
+<section id="slider" class="max-w-3xl mx-auto my-2 bg-white dark:bg-gray-800 text-center overflow-hidden sm:rounded-lg shadow-sm hidden">
+    <figure class="mt-5 flex justify-evenly items-center">
+        <x-secondary-button id="prev" class="h-8 m-5" onclick="Slider.prev()">
+            &lt;
+        </x-secondary-button>
 
-                <img
-                    id="slide"
-                    class="mx-6 max-w-60 sm:max-w-96 border-2 border-solid border-gray-400 dark:border-gray-600"
-                    src=""
-                    alt="{{ __('Slide') }}"
-                >
+        <img
+            id="slide"
+            class="max-w-60 sm:max-w-96 mx-6 border-2 border-solid border-gray-400 dark:border-gray-600"
+            src=""
+            alt="{{ __('Slide') }}"
+        >
 
-                <x-secondary-button id="next" class="m-5 h-8" onclick="Slider.next()">
-                    &gt;
-                </x-secondary-button>
-            </figure>
+        <x-secondary-button id="next" class="h-8 m-5" onclick="Slider.next()">
+            &gt;
+        </x-secondary-button>
+    </figure>
 
-            @if(session('document'))
-                <x-primary-button
-                    class="m-5 h-8"
-                    onclick="window.location.href = '{{ route('document.download-slider', ['document' => session('document')]) }}';"
-                >
-                    {{ __('documents.downloads.sliders.button') }}
-                </x-primary-button>
-            @endif
-        </div>
-    </div>
-</div>
+    @if(session('document'))
+        <x-primary-button
+            class="h-8 m-5"
+            onclick="window.location.href = '{{ route('document.download-slider', ['document' => session('document')]) }}';"
+        >
+            {{ __('documents.downloads.sliders.button') }}
+        </x-primary-button>
+    @endif
+</section>
