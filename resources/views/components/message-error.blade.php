9b62b8ff374d0a62f87ab3fc2f440a44fe354478
mezentsevs <mezentsevs@rambler.ru>
2025-07-17 16:54:55 +0400

    Refactor message error component

--- resources/views/components/message-error.blade.php
+++ resources/views/components/message-error.blade.php
@@ -1,9 +1,3 @@
-<div class="py-2">
-    <div class="max-w-3xl mx-auto">
-        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-2 text-red-600 dark:text-red-300">
-                {{ $slot }}
-            </div>
-        </div>
-    </div>
-</div>
+<section class="max-w-3xl mx-auto my-2 p-2 bg-white dark:bg-gray-800 text-red-600 dark:text-red-300 overflow-hidden sm:rounded-lg shadow-sm">
+    {{ $slot }}
+</section>
