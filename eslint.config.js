084bdc30bfd5260e24d918e76b277dd728589206
mezentsevs <mezentsevs@rambler.ru>
2025-07-25 17:38:55 +0400

    Add eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,62 @@
+import eslintPluginImport from 'eslint-plugin-import';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintPluginPromise from 'eslint-plugin-promise';
+import globals from 'globals';
+
+export default [
+    {
+        ignores: [
+            "dist/**",
+            "node_modules/**",
+            // "*.config.js"
+        ]
+    },
+    {
+        languageOptions: {
+            ecmaVersion: 2025,
+            sourceType: "module",
+            globals: {
+                ...globals.browser,
+                ...globals.node
+            }
+        },
+
+        plugins: {
+            import: eslintPluginImport,
+            promise: eslintPluginPromise,
+            prettier: eslintPluginPrettier
+        },
+
+        rules: {
+            "no-console": "warn",
+            "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+
+            "import/order": [
+                "error",
+                {
+                    groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+                    "newlines-between": "always"
+                }
+            ],
+
+            "indent": ["error", 4, {
+                "SwitchCase": 1,
+                "ignoredNodes": ["TemplateLiteral"]
+            }],
+
+            "prettier/prettier": [
+                "error",
+                {
+                    printWidth: 100,
+                    tabWidth: 4,
+                    useTabs: false,
+                    semi: true,
+                    singleQuote: true,
+                    trailingComma: "all",
+                    bracketSpacing: true,
+                    arrowParens: "avoid"
+                }
+            ]
+        }
+    }
+];
